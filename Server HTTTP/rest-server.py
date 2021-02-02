bfb5cfd53867553bb9be8833e7a1b5d64ed418f8
Gengigq96 <genisgq@gmail.com>
2021-02-02 21:30:51 +0100

    server with exams

--- Server HTTTP/rest-server.py
+++ Server HTTTP/rest-server.py
@@ -1,23 +1,100 @@
-#!flask/bin/python
+#!flask/bin/python3
 from flask import Flask, request
 from flask_restful import Resource, Api
-
+import json
 app = Flask(__name__)
 api = Api(app)
 
+
+x = {"exam": {  
+  "id": "1",    
+  "Questions": {  
+    "1": [
+  	  {"Question": "1+1="},
+      {"value": "2"},  
+      {"value": "1"},  
+      {"value": "3"}  
+    ],
+    "2": [
+  	  {"Question": "1+3="},
+      {"value": "2"},  
+      {"value": "1"},  
+      {"value": "4"}  
+    ],
+    "3": [
+  	  {"Question": "1+3="},
+      {"value": "2"},  
+      {"value": "1"},  
+      {"value": "4"}  
+    ]
+  }  
+}}
+y = {"exam": {  
+  "id": "2",    
+  "Questions": {  
+    "1": [
+  	  {"Question": "23+1="},
+      {"value": "24"},  
+      {"value": "1"},  
+      {"value": "3"}  
+    ],
+    "2": [
+  	  {"Question": "7+3="},
+      {"value": "10"},  
+      {"value": "1"},  
+      {"value": "4"}  
+    ],
+    "3": [
+  	  {"Question": "1+3="},
+      {"value": "4"},  
+      {"value": "1"},  
+      {"value": "2"}  
+    ]
+  }  
+}}
+z = {"exam": {  
+  "id": "3",    
+  "Questions": {  
+    "1": [
+  	  {"Question": "23+7="},
+      {"value": "30"},  
+      {"value": "1"},  
+      {"value": "3"}  
+    ],
+    "2": [
+  	  {"Question": "7+9="},
+      {"value": "16"},  
+      {"value": "1"},  
+      {"value": "4"}  
+    ],
+    "3": [
+  	  {"Question": "1+0="},
+      {"value": "4"},  
+      {"value": "1"},  
+      {"value": "2"}  
+    ]
+  }  
+}}
+
+examen1 = json.dumps(x) 
+examen2 = json.dumps(y)
+examen3 = json.dumps(z)
+exams = {1:examen1,2:examen2,3:examen3}
+
 class Index(Resource):
     def get(self):
-        return {"about":"Hello World"}
+        return json.dumps(x)
     def post(self):
         some_json = request.get_json()
         return {'You sent ': some_json}, 201
 
-class Multi(Resource):
+class Exams(Resource):
     def get(slef, num):
-        return {'result': num*10}
+        return (exams.get(num))
 
 api.add_resource(Index, '/')
-api.add_resource(Multi, '/multi/<int:num>')
+api.add_resource(Exams, '/exams/<int:num>')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
--- Server HTTTP/venv/bin/activate.fish
+++ Server HTTTP/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/genis/Escritorio/WS project/venv"
+set -gx VIRTUAL_ENV "/home/genis/Escritorio/WS project/Server HTTTP/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
