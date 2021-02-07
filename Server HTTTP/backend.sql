ef924803501657a12cdbf826aabc7d5ff43d7bbe
sydrex <loryyk95@hotmail.com>
2021-02-07 04:22:44 +0100

    basic functions

---
+++ Server HTTTP/backend.sql
@@ -0,0 +1,134 @@
+-- MySQL dump 10.17  Distrib 10.3.22-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: exams
+-- ------------------------------------------------------
+-- Server version	10.3.22-MariaDB-1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `exam`
+--
+
+DROP TABLE IF EXISTS `exam`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `exam` (
+  `id_exam` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) DEFAULT NULL,
+  `date` varchar(255) DEFAULT NULL,
+  `time` varchar(255) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_exam`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exam`
+--
+
+LOCK TABLES `exam` WRITE;
+/*!40000 ALTER TABLE `exam` DISABLE KEYS */;
+INSERT INTO `exam` VALUES (1,'description1','01/01/2021','120','UdL'),(2,'description1','01/01/2021','120','UdL'),(3,'description3','01/01/2021','120','UdL');
+/*!40000 ALTER TABLE `exam` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `grade`
+--
+
+DROP TABLE IF EXISTS `grade`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `grade` (
+  `id_student` varchar(4) NOT NULL,
+  `id_exam` int(11) NOT NULL,
+  `nota` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_student`,`id_exam`),
+  KEY `fk_exam2` (`id_exam`),
+  CONSTRAINT `fk_exam2` FOREIGN KEY (`id_exam`) REFERENCES `exam` (`id_exam`),
+  CONSTRAINT `fk_student` FOREIGN KEY (`id_student`) REFERENCES `student` (`id_student`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `grade`
+--
+
+LOCK TABLES `grade` WRITE;
+/*!40000 ALTER TABLE `grade` DISABLE KEYS */;
+/*!40000 ALTER TABLE `grade` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `question`
+--
+
+DROP TABLE IF EXISTS `question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `question` (
+  `id_question` int(11) NOT NULL AUTO_INCREMENT,
+  `id_exam` int(11) NOT NULL,
+  `question` varchar(255) DEFAULT NULL,
+  `answer1` varchar(255) DEFAULT NULL,
+  `answer2` varchar(255) DEFAULT NULL,
+  `answer3` varchar(255) DEFAULT NULL,
+  `correct_answer` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_question`,`id_exam`),
+  KEY `fk_exam` (`id_exam`),
+  CONSTRAINT `fk_exam` FOREIGN KEY (`id_exam`) REFERENCES `exam` (`id_exam`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `question`
+--
+
+LOCK TABLES `question` WRITE;
+/*!40000 ALTER TABLE `question` DISABLE KEYS */;
+INSERT INTO `question` VALUES (1,1,'34+1','35','10','20','1');
+/*!40000 ALTER TABLE `question` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `student` (
+  `id_student` varchar(4) NOT NULL,
+  PRIMARY KEY (`id_student`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-02-07  4:18:30
--- Server HTTTP/rest-server.py
+++ Server HTTTP/rest-server.py
@@ -1,100 +1,91 @@
 #!flask/bin/python3
 from flask import Flask, request
 from flask_restful import Resource, Api
+import mysql.connector
 import json
+import csv
+import sys
+
 app = Flask(__name__)
 api = Api(app)
 
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="admin",
+  password="Admin_1234",
+  database="exams"
+)
 
-x = {"exam": {  
-  "id": "1",    
-  "Questions": {  
-    "1": [
-  	  {"Question": "1+1="},
-      {"value": "2"},  
-      {"value": "1"},  
-      {"value": "3"}  
-    ],
-    "2": [
-  	  {"Question": "1+3="},
-      {"value": "2"},  
-      {"value": "1"},  
-      {"value": "4"}  
-    ],
-    "3": [
-  	  {"Question": "1+3="},
-      {"value": "2"},  
-      {"value": "1"},  
-      {"value": "4"}  
-    ]
-  }  
-}}
-y = {"exam": {  
-  "id": "2",    
-  "Questions": {  
-    "1": [
-  	  {"Question": "23+1="},
-      {"value": "24"},  
-      {"value": "1"},  
-      {"value": "3"}  
-    ],
-    "2": [
-  	  {"Question": "7+3="},
-      {"value": "10"},  
-      {"value": "1"},  
-      {"value": "4"}  
-    ],
-    "3": [
-  	  {"Question": "1+3="},
-      {"value": "4"},  
-      {"value": "1"},  
-      {"value": "2"}  
-    ]
-  }  
-}}
-z = {"exam": {  
-  "id": "3",    
-  "Questions": {  
-    "1": [
-  	  {"Question": "23+7="},
-      {"value": "30"},  
-      {"value": "1"},  
-      {"value": "3"}  
-    ],
-    "2": [
-  	  {"Question": "7+9="},
-      {"value": "16"},  
-      {"value": "1"},  
-      {"value": "4"}  
-    ],
-    "3": [
-  	  {"Question": "1+0="},
-      {"value": "4"},  
-      {"value": "1"},  
-      {"value": "2"}  
-    ]
-  }  
-}}
 
-examen1 = json.dumps(x) 
-examen2 = json.dumps(y)
-examen3 = json.dumps(z)
-exams = {1:examen1,2:examen2,3:examen3}
-
-class Index(Resource):
-    def get(self):
-        return json.dumps(x)
-    def post(self):
-        some_json = request.get_json()
-        return {'You sent ': some_json}, 201
 
 class Exams(Resource):
-    def get(slef, num):
-        return (exams.get(num))
+    def get(self):
 
-api.add_resource(Index, '/')
-api.add_resource(Exams, '/exams/<int:num>')
+        cursor_exam = mydb.cursor(dictionary=True)
+
+        cursor_exam.execute("SELECT * FROM exam")
+
+        allexams = cursor_exam.fetchall()
+
+        return (allexams)
+
+    def post(self):
+
+        data = request.get_json()
+
+        cursor_store_exam = mydb.cursor(dictionary=True)
+
+        sql = "INSERT INTO exam (description, date, time, location) VALUES (%s, %s, %s, %s)"
+        values = (data["description"], data["date"], data["time"], data["location"])
+
+        cursor_store_exam.execute(sql, values)
+
+        mydb.commit()
+
+        debug_store_exam = str(cursor_store_exam.rowcount) + " record inserted"
+
+        return debug_store_exam, 201
+
+
+class Exam(Resource):
+    #def get(self):
+
+    def get(self, id_exam):
+
+        cursor_exam = mydb.cursor(dictionary=True)
+
+        cursor_exam.execute("SELECT * FROM exam WHERE id_exam = "+id_exam+";")
+
+        concrete_exam = cursor_exam.fetchall()
+
+        return (concrete_exam)
+
+
+class Question(Resource):
+
+    def post(self, id_exam):
+
+        data = request.get_json()
+
+        cursor_store_exam = mydb.cursor(dictionary=True)
+
+        sql = "INSERT INTO question (id_exam, question, answer1, answer2, answer3, correct_answer) VALUES (%s, %s, %s, %s, %s, %s)"
+        values = (id_exam, data["question"], data["answer1"], data["answer2"], data["answer3"], data["correct_answer"])
+
+        cursor_store_exam.execute(sql, values)
+
+        mydb.commit()
+
+        debug_store_exam = str(cursor_store_exam.rowcount) + " record inserted"
+
+        return debug_store_exam, 201
+
+
+
+api.add_resource(Exams, '/exams/')
+api.add_resource(Exam, '/exams/<string:id_exam>')
+api.add_resource(Question, '/question/<int:id_exam>')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
