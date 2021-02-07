cfd979045480d1f7d8c4f49fdc99fa7ee81cbda5
sydrex <loryyk95@hotmail.com>
2021-02-07 14:40:48 +0100

    server updated with new functionality

--- Server HTTTP/rest-server.py
+++ Server HTTTP/rest-server.py
@@ -60,6 +60,41 @@ class Exam(Resource):
 
         return (concrete_exam)
 
+    def post(self, id_exam):
+
+        data = request.get_json()
+
+        cursor_modify_desc_exam = mydb.cursor(dictionary=True)
+
+        sql = "UPDATE exam SET description = '"+(data["description"])+"' WHERE id_exam = "+id_exam+";"
+
+
+        cursor_modify_desc_exam.execute(sql)
+
+        mydb.commit()
+
+        debug_modify_desc_exam = str(cursor_modify_desc_exam.rowcount) + " record updated"
+
+        return debug_modify_desc_exam, 201
+
+    def delete(self, id_exam):
+
+
+        cursor_delete_exam = mydb.cursor(dictionary=True)
+
+        cursor_delete_exam.execute("DELETE FROM question WHERE id_exam = "+id_exam+"; ")
+
+        debug_delete_question = cursor_delete_exam.rowcount
+
+        cursor_delete_exam.execute("DELETE FROM exam WHERE id_exam = "+id_exam+";")
+
+        mydb.commit()
+
+        debug_delete_exam = str(cursor_delete_exam.rowcount + debug_delete_question) + " record(s) deleted"
+
+        cursor_delete_exam.close()
+
+        return debug_delete_exam, 201
 
 class Question(Resource):
 
@@ -81,10 +116,36 @@ class Question(Resource):
         return debug_store_exam, 201
 
 
+# class ExamByDesc(Resource):
+#
+#     def get(self, txtSearch):
+#
+#         cursor_search_exam = mydb.cursor(dictionary=True)
+#
+#         cursor_search_exam.execute("SELECT * FROM exam WHERE description = \"*"+txtSearch+"*\";")
+#
+#         result_exams = cursor_search_exam.fetchall()
+#
+#         return (result_exams)
 
+
+#LIST ALL EXAMS (GET) // POST AN EXAM (POST)
 api.add_resource(Exams, '/exams/')
+#LIST AN SPECIFIC EXAM (GET) // MODIFY DESC EXAM (POST) // DELETE EXAM (DELETE)
 api.add_resource(Exam, '/exams/<string:id_exam>')
+#POST A QUESTION OF AN SPECIFIC EXAM (POST)
 api.add_resource(Question, '/question/<int:id_exam>')
+#SEARCH EXAM BY FULL/PARTIAL DESC (GET)
+#api.add_resource(ExamByDesc, '/exams/<string:txtSearch>')
+
+#******
+#SEARCH EXAM BY FULL/PARTIAL DESC (GET)
+# api.add_resource(SearchExam, '/exams/<string:desc>')
+# #UPLOAD (POST) AND DOWNLOAD (GET) STUDENTS GRADES
+# api.add_resource(Grades, '/grades/<string:id_exam>')
+# #MANAGE STUDENTS (GET)
+# api.add_resource(Student, '/students/<string:id_student>')
+#******
 
 
 if __name__ == '__main__':
